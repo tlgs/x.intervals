8ec754d70ead393edfb6d5f78c56322e474ce6d3
Tiago Seabra <tlgs@users.noreply.github.com>
2023-11-08 17:12:44 +0000

    Add globals to ESLint config
    Also, disable `no-labels` and `no-unused-expressions` rules as these are
    very helpful when writing short scripts.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
-  extends: [
-    'semistandard'
-  ]
+  extends: 'semistandard',
+  globals: {
+    'icu': 'readonly',
+    'activity': 'readonly',
+    'interval': 'readonly',
+    'data': 'readonly',
+  },
+  rules: {
+    'no-labels': 'off',
+    'no-unused-expressions': 'off',
+  }
 };
