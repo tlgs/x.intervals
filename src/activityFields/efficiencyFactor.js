a46faf455c686ae4d5e27d4adc99d0a56e67c071
Tiago Seabra <tlgs@users.noreply.github.com>
2024-02-19 23:50:23 +0000

    Fix ef field for "marked as done" workouts
    Make sure to emit a `null` instead of a `NaN`

--- src/activityFields/efficiencyFactor.js
+++ src/activityFields/efficiencyFactor.js
@@ -4,6 +4,10 @@ out: {
     break out;
   }
 
+  if (activity.gap === null || activity.average_heartrate === null) {
+    break out;
+  }
+
   const speed = activity.gap * 60; // meters per minute
   speed / activity.average_heartrate;
 }
